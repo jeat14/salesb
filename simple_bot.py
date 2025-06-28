5587534a8657547bd95a0142ac3114daf3ec0f98
jeat14 <jeat425@gmail.com>
2025-06-28 22:09:38 +0100

    Update simple_bot.py

--- simple_bot.py
+++ simple_bot.py
@@ -7,7 +7,7 @@ import json
 from datetime import datetime
 
 # Initialize bot with your token
-TOKEN = '8108658761:AAE_2O5d8zstSITUiMoN9jBK2oyGRRg7QX1'
+TOKEN = '8060770660:AAHh2Y1YH0GR2F6hIhC3Ip3r5RIN1xtcgcE'
 bot = telebot.TeleBot(TOKEN)
 
 # Admin user IDs
