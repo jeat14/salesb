cf60e2abedf76531fd2e539f11b9dc2167535217
jeat14 <jeat425@gmail.com>
2025-06-30 23:35:30 +0100

    Update simple_bot.py

--- simple_bot.py
+++ simple_bot.py
@@ -6,7 +6,7 @@ import uuid
 from datetime import datetime
 
 # --- CONFIGURATION ---
-TOKEN = '8108658761:AAE_2O5d8zstSITUiMoN9jBK2oyGRRg7QX8' # Replace with your bot's token
+TOKEN = '8060770660:AAHh2Y1YH0GR2F6hIhC3Ip3r5RIN1xtcgcE' # Replace with your bot's token
 ADMIN_IDS = [
     7481885595,  # @packoa's ID
     7864373277,  # @xenslol's ID
